9dd2a7fe91c940d243945fed014f9d803faa22c8
logonrm <logonrm@fiap.com.br>
2018-03-10 17:17:17 -0300

    Update

--- app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosFragment.kt
+++ app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosFragment.kt
@@ -2,17 +2,53 @@ package com.example.logonrm.carros.ui.listacarros
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.logonrm.carros.R
+import com.example.logonrm.carros.api.CarroAPI
+import com.example.logonrm.carros.api.RetrofitClient
+import com.example.logonrm.carros.model.Carro
+import kotlinx.android.synthetic.main.fragment_lista_carros.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListaCarrosFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        carregarDados()
 
         return inflater!!.inflate(R.layout.fragment_lista_carros, container, false)
+    }
+    fun carregarDados(){
+        val api = RetrofitClient
+                .getInstance(baseUrl = "https://carroapiscjmateus.herokuapp.com")
+                .create(CarroAPI::class.java)
+
+        api.buscarTodos().enqueue(object : Callback<List<Carro>> {
+            override fun onFailure(call: Call<List<Carro>>?,
+                                   t: Throwable?) {
+
+            }
+
+            override fun onResponse(call: Call<List<Carro>>?,
+                                    response: Response<List<Carro>>?) {
+                if(response?.isSuccessful == true){
+                    setupLista(response?.body())
+                }
+            }
+        })
+    }
+    fun setupLista(carros: List<Carro>?) {
+        carros.let {
+            rvCarros.adapter = ListaCarrosAdapter(carros!!, context)
+            val layoutManager = LinearLayoutManager(context)
+            rvCarros.layoutManager = layoutManager
+        }
+
     }
 }
