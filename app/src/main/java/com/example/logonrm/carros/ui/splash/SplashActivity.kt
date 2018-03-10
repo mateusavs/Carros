82d75ac9ce27d35b4bf8c4952f2bd0533218a3f9
logonrm <logonrm@fiap.com.br>
2018-03-10 15:06:04 -0300

    Update

--- app/src/main/java/com/example/logonrm/carros/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/logonrm/carros/ui/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
-package com.example.logonrm.carros
+package com.example.logonrm.carros.ui.splash
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import com.example.logonrm.carros.ui.main.MainActivity
+import com.example.logonrm.carros.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
