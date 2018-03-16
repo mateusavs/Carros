8e9f414581c794278a17cbfc88514b9192539043
logonrm <logonrm@fiap.com.br>
2018-03-15 21:19:59 -0300

    App

--- app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosAdapter.kt
+++ app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.logonrm.carros.R
 import com.example.logonrm.carros.model.Carro
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_carro.view.*
 
 class ListaCarrosAdapter(private val carros: List<Carro>,
@@ -32,6 +33,7 @@ class ListaCarrosAdapter(private val carros: List<Carro>,
         fun bindView(carro: Carro){
             itemView.tvMarca.text = carro.marca
             itemView.tvModelo.text = carro.modelo
+            Picasso.get().load(carro.urlImagem).placeholder(R.drawable.'').error(R.drawable.errou).into(itemView.ivFoto)
         }
     }
 }
--- app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosFragment.kt
+++ app/src/main/java/com/example/logonrm/carros/ui/listacarros/ListaCarrosFragment.kt
@@ -11,7 +11,9 @@ import com.example.logonrm.carros.R
 import com.example.logonrm.carros.api.CarroAPI
 import com.example.logonrm.carros.api.RetrofitClient
 import com.example.logonrm.carros.model.Carro
+import kotlinx.android.synthetic.main.erro.*
 import kotlinx.android.synthetic.main.fragment_lista_carros.*
+import kotlinx.android.synthetic.main.loading.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,26 +22,41 @@ class ListaCarrosFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        carregarDados()
-
         return inflater!!.inflate(R.layout.fragment_lista_carros, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        carregarDados()
     }
     fun carregarDados(){
         val api = RetrofitClient
-                .getInstance(baseUrl = "https://carroapiscjmateus.herokuapp.com")
+                .getInstance()
                 .create(CarroAPI::class.java)
+
+        loading.visibility = View.VISIBLE
 
         api.buscarTodos().enqueue(object : Callback<List<Carro>> {
             override fun onFailure(call: Call<List<Carro>>?,
                                    t: Throwable?) {
+                containerErro.visibility = View.VISIBLE
+                tvMensagemErro.text = t?.message
+                loading.visibility = View.GONE
 
             }
 
             override fun onResponse(call: Call<List<Carro>>?,
                                     response: Response<List<Carro>>?) {
+                containerErro.visibility = View.GONE
+                tvMensagemErro.text = ""
+
                 if(response?.isSuccessful == true){
                     setupLista(response?.body())
+                } else{
+                    containerErro.visibility = View.VISIBLE
+                    tvMensagemErro.text = response?.errorBody()?.charStream()?.readText()
                 }
+                loading.visibility = View.GONE
             }
         })
     }
